8826df1cfacc2070c7eab377fdd500374094035a
shanbing <shanbing@seeapp.com>
2018-07-02 18:41:59 +0800

    添加配置中心服务端

--- src/main/java/top/shanbing/App.java
+++ src/main/java/top/shanbing/App.java
@@ -2,10 +2,12 @@ package top.shanbing;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class App {
 
     public static void main(String[] args) {
